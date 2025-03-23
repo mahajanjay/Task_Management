788a2d885ca0fad24d9e074d7908c5270efbbbce
Jayant Mahajan <jayantmahajan99@gmail.com>
2025-03-23 19:24:48 +0530

    Removes weatherForcast controller.

--- task_management.client/src/app/app.component.ts
+++ task_management.client/src/app/app.component.ts
@@ -14,24 +14,9 @@ interface WeatherForecast {
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+  ngOnInit() { }
 
   title = 'task_management.client';
 }
