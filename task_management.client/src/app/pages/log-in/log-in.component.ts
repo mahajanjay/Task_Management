2ea7d80d579cc01b75949e42350500c3152091d8
Jayant Mahajan <jayantmahajan99@gmail.com>
2025-07-31 21:14:02 +0530

    Adds login page UI

--- task_management.client/src/app/pages/log-in/log-in.component.ts
+++ task_management.client/src/app/pages/log-in/log-in.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+  loginForm: FormGroup;
+  isSubmitting = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isSubmitting = true;
+      console.log('Login form submitted:', this.loginForm.value);
+      // Add your login logic here
+      setTimeout(() => {
+        this.isSubmitting = false;
+      }, 2000);
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  markFormGroupTouched() {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  getFieldError(fieldName: string): string {
+    const field = this.loginForm.get(fieldName);
+    if (field?.invalid && field?.touched) {
+      if (field.errors?.['required']) {
+        return `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} is required`;
+      }
+      if (field.errors?.['email']) {
+        return 'Please enter a valid email address';
+      }
+      if (field.errors?.['minlength']) {
+        const requiredLength = field.errors['minlength'].requiredLength;
+        return `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} must be at least ${requiredLength} characters`;
+      }
+    }
+    return '';
+  }
 }
