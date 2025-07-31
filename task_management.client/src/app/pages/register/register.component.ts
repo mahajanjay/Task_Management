882b9deb1648be95bb9fcac4ef9e3f4b42570640
Jayant Mahajan <jayantmahajan99@gmail.com>
2025-07-31 21:07:32 +0530

    Adds register page UI

--- task_management.client/src/app/pages/register/register.component.ts
+++ task_management.client/src/app/pages/register/register.component.ts
@@ -1,11 +1,82 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  isSubmitting = false;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    }
+    
+    if (confirmPassword && confirmPassword.errors) {
+      delete confirmPassword.errors['passwordMismatch'];
+      if (Object.keys(confirmPassword.errors).length === 0) {
+        confirmPassword.setErrors(null);
+      }
+    }
+    
+    return null;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.isSubmitting = true;
+      console.log('Register form submitted:', this.registerForm.value);
+      // Add your registration logic here
+      setTimeout(() => {
+        this.isSubmitting = false;
+      }, 2000);
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  markFormGroupTouched() {
+    Object.keys(this.registerForm.controls).forEach(key => {
+      const control = this.registerForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  getFieldError(fieldName: string): string {
+    const field = this.registerForm.get(fieldName);
+    if (field?.invalid && field?.touched) {
+      if (field.errors?.['required']) {
+        return `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} is required`;
+      }
+      if (field.errors?.['email']) {
+        return 'Please enter a valid email address';
+      }
+      if (field.errors?.['minlength']) {
+        const requiredLength = field.errors['minlength'].requiredLength;
+        return `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} must be at least ${requiredLength} characters`;
+      }
+      if (field.errors?.['passwordMismatch']) {
+        return 'Passwords do not match';
+      }
+    }
+    return '';
+  }
 }
