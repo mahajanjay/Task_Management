9344c83d25762d13f1fd6a970a583ed2ee561cc7
Jayant Mahajan <jayantmahajan99@gmail.com>
2025-08-26 22:21:20 +0530

    Adds functionality to show logged in user at app initialization

--- task_management.client/src/app/core/components/header/header.component.ts
+++ task_management.client/src/app/core/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, Input } from '@angular/core';
 import { HeaderService } from '../../../services/core/header.service';
 import { THEME } from '../../../shared/constants/core';
 import { ThemeService } from '../../../services/core/theme.service';
+import { LoggedInUserService } from '../../../services/core/logged-in-user.service';
 
 @Component({
   selector: 'app-header',
@@ -18,9 +19,12 @@ export class HeaderComponent {
 
   public headerService = inject(HeaderService);
   public themeService = inject(ThemeService);
+  public loggedInUserService = inject(LoggedInUserService);
 
   ngOnInit() {
     this.isDarkMode = this.themeService.isDarkMode();
+    console.log(this.loggedInUserService.getLoggedInUser());
+    
   }
 
   toggleTheme() {
--- task_management.client/src/main.ts
+++ task_management.client/src/main.ts
@@ -4,12 +4,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { inject, provideAppInitializer } from '@angular/core';
+import { LoggedInUserService } from './app/services/core/logged-in-user.service';
+import { getLocalStorage } from './app/shared/utils/storage';
+import { TOKEN } from './app/shared/constants/core';
+
+const storeLoggedInUserFactory = () => {
+  const loggedInUserService = inject(LoggedInUserService);
+  const token = getLocalStorage(TOKEN);
+  if (token) {
+    loggedInUserService.setLoggedInUser(token);
+  }
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    ),
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAppInitializer(storeLoggedInUserFactory),
+  ],
 });
